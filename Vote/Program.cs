7f70d1052ec05c602a76065818427a16b64ef3b4
shubhampachghare <shubhampachghare@gmail.com>
2023-06-28 22:54:51 +0530

    [Shubham] -VoteCast

---
+++ Vote/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthNumber
+{
+    class Vote
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter To Age check Voting Eligibility");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a >= 18)
+            {
+                Console.WriteLine("You are Eligible to cast your vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not Eligible");
+            }
+        }
+    }
+}
